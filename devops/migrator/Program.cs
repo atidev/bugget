ec81dc77c3bb0ffce5cadc578a49225ff0b21e6f
aleksandr.z <gently.whitesnow@outlook.com>
2025-03-30 22:30:17 +0300

    init migrator

---
+++ devops/migrator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using DbUp;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .Build();
+
+var connectionString =
+    args.FirstOrDefault()
+    ?? config["POSTGRES_CONNECTION_STRING"];
+
+var upgrader =
+    DeployChanges.To
+        .PostgresqlDatabase(connectionString)
+        .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+        // миграция выполняется в транзакции (по умолчанию)
+        .WithTransaction()
+        .LogToConsole()
+        .Build();
+
+var result = upgrader.PerformUpgrade();
+
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error);
+    Console.ResetColor();
+#if DEBUG
+    Console.ReadLine();
+#endif                
+    return -1;
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Success!");
+Console.ResetColor();
+return 0;
---
+++ devops/migrator/readme.md
@@ -0,0 +1,4 @@
+# мигратор
+
+docker-compose -f docker-compose-test.yml build --no-cache
+docker-compose -f docker-compose-test.yml up
