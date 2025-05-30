8c3bdafbc5840c619eea1b6dab7c6b546b1629eb
aleksandr.z <gently.whitesnow@outlook.com>
2025-05-30 12:17:34 +0300

    configuration refactoring

--- backend/bugget-api/Bugget/Program.cs
+++ backend/bugget-api/Bugget/Program.cs
@@ -23,19 +23,14 @@ using Bugget.Configurations;
 using SixLabors.ImageSharp;
 using OpenTelemetry.Metrics;
 using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.Network;
+using Bugget.Extensions;
+using Bugget.Logging;
 
-Log.Logger = new LoggerConfiguration()          // «bootstrap»-логгер
-    .WriteTo.Console()
-    .CreateLogger();
+Log.Logger = BootstrapLogger.Create();
 
 try
 {
-    Log.Information("Host starting");
-
     var builder = WebApplication.CreateBuilder(args);
-
 
     builder.Configuration
         .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
@@ -53,136 +48,23 @@ try
         Console.WriteLine("File external_settings.json not found");
     }
 
-    builder.Host.UseSerilog((ctx, lc) =>
-        lc.ReadFrom.Configuration(ctx.Configuration)); 
-
-    builder.Services.AddSingleton<IUserIdProvider, SignalRUserIdProvider>();
-    builder.Services.AddSignalR(options =>
-        {
-            options.EnableDetailedErrors = true; // Показывать ошибки в логе
-            options.KeepAliveInterval = TimeSpan.FromSeconds(15); // Пинг каждые 15 сек
-            options.ClientTimeoutInterval = TimeSpan.FromSeconds(60); // Клиент ждёт 60 сек перед разрывом
-        })
-        .AddJsonProtocol(options =>
-        {
-            options.PayloadSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-        })
-        .AddHubOptions<ReportPageHub>(options => { options.AddFilter<HubExceptionHandlerFilter>(); });
-
-    // разрешаем cors для локального тестирования
-    builder.Services.AddCors(options =>
-    {
-        options.AddPolicy("CorsPolicy", policy =>
-        {
-            policy.SetIsOriginAllowed(_ => true) // Разрешает все Origins, но с `AllowCredentials()`
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .AllowCredentials(); // Разрешаем куки и WebSockets
-        });
-    });
-
-    builder.Services.Configure<FileStorageOptions>(
-        builder.Configuration.GetSection(nameof(FileStorageOptions)));
-    builder.Services.Configure<AuthHeadersOptions>(
-        builder.Configuration.GetSection("ExternalSettings:Authentication"));
-
-    builder.Services.AddAutoMapper(typeof(Bugget.Entities.MappingProfiles.BugMappingProfile).Assembly);
-
-    builder.Services.AddExternalClients();
-
     builder.Services
-        .AddSingleton<ReportsService>()
-        .AddSingleton<BugsService>()
-        .AddSingleton<BugsEventsService>()
-        .AddSingleton<AttachmentObsoleteService>()
-        .AddSingleton<ReportEventsService>()
-        .AddSingleton<ReportAutoStatusService>()
-        .AddSingleton<ParticipantsService>()
-        .AddSingleton<AttachmentOptimizator>()
-        .AddSingleton<AttachmentService>()
-        .AddSingleton<AttachmentEventsService>()
-        .AddSingleton<ImageOptimizeWriter>()
-        .AddSingleton<TextOptimizeWriter>()
-        .AddSingleton<AttachmentOptimizator>()
-        .AddSingleton<IAttachmentKeyGenerator, LocalAttachmentKeyGenerator>()
-        .AddSingleton<CommentsObsoleteService>()
-        .AddSingleton<CommentsService>()
-        .AddSingleton<CommentEventsService>()
-        .AddSingleton<LimitsService>();
-
-    builder.Services
-        .AddSingleton<ReportsDbClient>()
-        .AddSingleton<CommentsObsoleteDbClient>()
-        .AddSingleton<CommentsDbClient>()
-        .AddSingleton<BugsDbClient>()
-        .AddSingleton<AttachmentObsoleteDbClient>()
-        .AddSingleton<AttachmentDbClient>()
-        .AddSingleton<EmployeesFileClient>()
-        .AddSingleton<TeamsFileClient>()
-        .AddSingleton<ParticipantsDbClient>()
-        .AddSingleton<IEmployeesClient>((sp) => sp.GetRequiredService<EmployeesFileClient>())
-        .AddSingleton<ITeamsClient>((sp) => sp.GetRequiredService<TeamsFileClient>())
-        .AddSingleton<IFileStorageClient, LocalFileStorageClient>();
-
-    builder.Services.AddHostedService((sp) => sp.GetRequiredService<EmployeesFileClient>());
-    builder.Services.AddHostedService((sp) => sp.GetRequiredService<TeamsFileClient>());
-
-    builder.Services.AddHealthChecks();
-    builder.Services.AddAuthHeaders();
-    builder.Services.AddSingleton<ITaskQueue, TaskQueue.TaskQueue>()
-        .AddHostedService(provider => (TaskQueue.TaskQueue)provider.GetRequiredService<ITaskQueue>());
-
-    builder.Services.AddSwaggerConfiguration(builder.Configuration);
-
-    builder.Services.AddSingleton<ResultExceptionHandlerMiddleware>();
-
-    builder.Services.AddControllers((options) =>
-    {
-        var policy = new AuthorizationPolicyBuilder()
-            .AddAuthenticationSchemes(AuthSchemeNames.Headers)
-            .RequireAuthenticatedUser()
-            .Build();
-
-        options.Filters.Add(new AuthorizeFilter(policy));
-    })
-    .AddJsonOptions(options => { options.JsonSerializerOptions.PropertyNamingPolicy = SnakeCaseNamingPolicy.Instance; })
-        .ConfigureApiBehaviorOptions(o =>
-            o.InvalidModelStateResponseFactory = _ => new ModelStateInvalidHandler());
-
-    builder.Services.AddSingleton<IReportPageHubClient, ReportPageHubClient>();
-
-    builder.Services.AddOpenTelemetry()
-        .WithMetrics(metrics => metrics
-            .AddRuntimeInstrumentation()
-            .AddAspNetCoreInstrumentation()
-            .AddHttpClientInstrumentation()
-            .AddPrometheusExporter());
+        .AddConfiguration(builder.Configuration)
+        .AddLogging(builder.Configuration)
+        .AddDataAccess(builder.Configuration)
+        .AddBusinessLogic()
+        .AddMessaging()
+        .AddWebApi(builder.Configuration, builder.Environment);
 
     #region заклинания
-    // для маппинга snake_case в c# типы средствами dapper
     Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
-    // настройка image sharp
-    Configuration.Default.MaxDegreeOfParallelism = Environment.ProcessorCount; // multi-threaded encode
-    Configuration.Default.PreferContiguousImageBuffers = true; // меньше аллокаций в обработке
+    Configuration.Default.MaxDegreeOfParallelism = Environment.ProcessorCount;
+    Configuration.Default.PreferContiguousImageBuffers = true;
     #endregion
 
     var app = builder.Build();
-
-    app.UseSwaggerConfiguration();
-    app.UseSerilogRequestLogging();
-
-    app.MapHealthChecks("/_internal/ping");
-    app.UseOpenTelemetryPrometheusScrapingEndpoint();
-    app.MapControllers();
-
-    app.UseCors("CorsPolicy");
-
-    app.UseMiddleware<ResultExceptionHandlerMiddleware>();
-
-    app.MapHub<ReportPageHub>("/v1/report-page-hub"); // Подключаем хаб
-
+    app.UsePipeline();
     app.Run();
-
 }
 catch (Exception ex)
 {
@@ -191,15 +73,4 @@ catch (Exception ex)
 finally
 {
     await Log.CloseAndFlushAsync();
-}
-
-
-public class SnakeCaseNamingPolicy : JsonNamingPolicy
-{
-    public static readonly SnakeCaseNamingPolicy Instance = new();
-
-    public override string ConvertName(string name)
-    {
-        return Regex.Replace(name, "([a-z0-9])([A-Z])", "$1_$2").ToLower();
-    }
 }
