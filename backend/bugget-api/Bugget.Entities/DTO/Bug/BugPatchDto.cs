44ec735e0d6700abc55102a81a93c300871acb8e
Александр Зайцев <34779498+gently-whitesnow@users.noreply.github.com>
2025-09-18 14:10:11 +0300

    Увеличение размера текстовых полей (#52)
    * Fix pre-commit: use npx for prettier in lint-staged
    
    * Change text length in comments and bugs
    
    * rollback

--- backend/bugget-api/Bugget.Entities/DTO/Bug/BugPatchDto.cs
+++ backend/bugget-api/Bugget.Entities/DTO/Bug/BugPatchDto.cs
@@ -4,9 +4,9 @@ namespace Bugget.Entities.DTO.Bug;
 
 public sealed class BugPatchDto
 {
-    [StringLength(512, MinimumLength = 1)]
+    [StringLength(2048, MinimumLength = 1)]
     public string? Receive { get; init; }
-    [StringLength(512, MinimumLength = 1)]
+    [StringLength(2048, MinimumLength = 1)]
     public string? Expect { get; init; }
     [Range(0, 2)]
     public int? Status { get; init; }
