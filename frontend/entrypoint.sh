16c2ca1f5f4aef4b52c50d4f07964c9a7996b039
Victoria Ivanova <victoria.i@ati.su>
2025-09-24 19:51:32 +0400

    remove changes from entrypoint

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
 # Заменяем плейсхолдеры в env.js на реальные значения
 sed -i "s|__API_URL__|${API_URL}|g" /usr/share/nginx/html/env.js
-sed -i "s|__BASE_PATH__|${BASE_PATH:-/}|g" /usr/share/nginx/html/env.js
 
 # Запускаем Nginx
 exec nginx -g "daemon off;"
