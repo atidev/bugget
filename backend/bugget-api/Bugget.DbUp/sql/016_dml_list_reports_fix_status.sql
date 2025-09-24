7eb44d4b9e5eb966d9681dbcc58be1ab8b541090
aleksandr.z <gently.whitesnow@outlook.com>
2025-09-24 13:39:14 +0300

    fix sql

--- backend/bugget-api/Bugget.DbUp/sql/016_dml_list_reports_fix_status.sql
+++ backend/bugget-api/Bugget.DbUp/sql/016_dml_list_reports_fix_status.sql
@@ -10,7 +10,7 @@ BEGIN
     RETURN QUERY
         SELECT public.get_report(r.id)
         FROM public.reports r
-        WHERE r.status = 0 or r.status = 2
+        WHERE (r.status = 0 or r.status = 2)
           AND r.id IN (SELECT report_id
                        FROM public.report_participants
                        WHERE user_id = _user_id);
