f0c54ed651296669979652d0aa88383f84fd4811
aleksandr.z <gently.whitesnow@outlook.com>
2025-10-08 22:02:51 +0300

    fix env initialization

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
-import App from "./App.tsx";
 import { loadEnvScript } from "@/utils/envLoader";
 
-// Initialize app after env.js is loaded
-loadEnvScript().then(() => {
+// инициализируем приложение после загрузки env.js
+loadEnvScript().then(async () => {
+  // Откладываем импорт App до тех пор, пока runtime env не станет доступным
+  const App = (await import("./App.tsx")).default;
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <App />
